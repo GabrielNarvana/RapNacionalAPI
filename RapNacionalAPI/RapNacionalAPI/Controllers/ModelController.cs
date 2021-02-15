23ae971f1497a86c8f34e335cbb61ec572463bfc
GabrielNarvana <gabriel.j.l@hotmail.com>
2021-02-15 18:41:52 -0300

    Initial commit

---
+++ RapNacionalAPI/RapNacionalAPI/Controllers/ModelController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace RapNacionalAPI.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ModelController : ControllerBase
+    {
+        private readonly ILogger<ModelController> _logger;
+
+        public ModelController(ILogger<ModelController> logger)
+        {
+            _logger = logger;
+        }
+    }
+}
