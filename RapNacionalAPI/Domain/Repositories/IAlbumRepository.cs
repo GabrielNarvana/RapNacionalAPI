7db9f520a65478ee4d1321548f8e3d16f359a678
GabrielNarvana <gabriel.j.l@hotmail.com>
2021-02-15 20:58:11 -0300

    Base repository

---
+++ RapNacionalAPI/Domain/Repositories/IAlbumRepository.cs
@@ -0,0 +1,14 @@
+﻿using RapNacionalAPI.Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RapNacionalAPI.Domain.Repositories
+{
+    public interface IAlbumRepository
+    {
+        public List<dynamic> GetAll();
+        public int Insert(Album album);
+    }
+}
